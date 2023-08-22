d92b88c5617cf054d420a8bfed7d1290e23b1948
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-08-21 23:26:40 -0700

    Moved comment for fixture

--- test_core_utilities.py
+++ test_core_utilities.py
@@ -8,7 +8,8 @@ import platform
 
 class TestCoreUtilities:
 
-    @pytest.fixture(autouse=True) # This fixture will be used by all tests in this class
+    # This fixture will be used by all tests in this class
+    @pytest.fixture(autouse=True)
     def setup(self) -> None:
         """Setup the test class.
         """
