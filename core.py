e9568f2ce97167e28f263884632ab9aefa11d0dc
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-08-26 14:40:11 -0700

    Formatted docstring

--- core.py
+++ core.py
@@ -64,7 +64,7 @@ class Utilities:
 
     def generate_timestamp_YYYY_MM_DD_T_MM(self) -> str:
         """Generate a timestamp in the format of YYYYMMDDTHHMM
-        
+
         Example:
             Year Month Day Time Hour Minute Second (e.g 20210101T120000)
         Returns:
