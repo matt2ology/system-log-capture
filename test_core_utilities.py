5d55d2f240e3b360e713f6dae0714aa5f9c37925
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-08-25 22:20:46 -0700

    Formatted expected_path assignment value

--- test_core_utilities.py
+++ test_core_utilities.py
@@ -45,8 +45,10 @@ class TestCoreUtilities:
         mock_glob: Mock = Mock(return_value=[self.desktop_path] * num_paths)
         monkeypatch.setattr("core.glob", mock_glob)
 
-        expected_path: str = self.desktop_path if num_paths == 1 else os.path.join(
-            os.path.expanduser("~"), "Desktop")
+        expected_path: str = (
+            self.desktop_path if num_paths == 1 else os.path.join(
+                os.path.expanduser("~"), "Desktop")
+        )
         assert self.core.get_desktop_path() == expected_path
 
     def test_normalize_path(self) -> None:
