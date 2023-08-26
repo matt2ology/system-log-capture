6334857540fc9a870e8c8bbd9068718a7eba8d6d
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-08-26 12:25:35 -0700

    Added reset directory button

--- gui.py
+++ gui.py
@@ -40,6 +40,7 @@ class GUI:
         self._create_output_dir_entry()
         self._create_browse_button()
         self._create_capture_button()
+        self._create_reset_directory_path_button()
 
     def _create_app_h1_header(self) -> None:
         """Create the h1 header of the app.
@@ -109,6 +110,30 @@ class GUI:
         # entry with the selected directory
         if output_dir:
             self.default_directory.set(value=output_dir)
+
+    def _create_reset_directory_path_button(self) -> None:
+        """Create the reset directory path button. This button resets the
+        output directory path to the default directory path.
+        """
+        reset_directory_path_button = Button(
+            self.frame,
+            text="Reset Directory Path",
+            font=("helvetica", 12),
+            bg="pink",
+            fg="black",
+            activebackground="red",
+            activeforeground="white",
+            cursor="hand1",
+            command=self._reset_directory_path_button_clicked
+        )
+        reset_directory_path_button.grid(row=1, column=6, pady=10, padx=10)
+
+    def _reset_directory_path_button_clicked(self) -> None:
+        """This function is called when the reset directory
+        path button is clicked.
+        self: GUI
+        """
+        self.default_directory.set(value=core_utilities().get_desktop_path())
 
     def _create_capture_button(self) -> None:
         """Create the capture button and is centered in the frame.
